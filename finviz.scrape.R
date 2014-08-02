9365232e1ec03698b956add0f0f02d42bf2ccf6d
shawn0122 <shawnpope@gmail.com>
2014-08-02 13:22:02 -0400

    Update finviz.scrape.R

--- finviz.scrape.R
+++ finviz.scrape.R
@@ -1,18 +1,28 @@
 library(XML)
-stock="AAPL"
-finviz.url <- paste("http://finviz.com/quote.ashx?t=", stock, sep="")
-tables <- readHTMLTable(finviz.url)
 
+process.fin.data<-function(stock){
+  
+  finviz.url <- paste("http://finviz.com/quote.ashx?t=", stock, sep="")
+  tables <- readHTMLTable(finviz.url)
+    data=as.data.frame(tables[6])
+    data=data[13:24,]
 
-data=as.data.frame(tables[6])
-data=data[13:24,]
+    final.data=as.data.frame(unlist(cbind(data[,c(2,4,6,8,10,12)])))
+    final.data=t(final.data)
+  
+    # Check to make sure download is in right format 
+    if(ncol(final.data)==72){
+      cat(stock, ": processed correctly \n")
+    } else (cat(stock, ": !error investigate \n"))
 
-final.data=as.data.frame(unlist(cbind(data[,c(2,4,6,8,10,12)])))
-final.data=t(final.data)
-dim(final.data)
+    # Format
+    colnames(final.data)=unlist(cbind(data[,c(1,3,5,7,9,11)]))
+  
+    return(final.data)
+}
 
-colnames(final.data)=unlist(cbind(data[,c(1,3,5,7,9,11)]))
+# PASS A VECTOR OF STOCKS
+output<-rbind(process.fin.data("AAPL"),
+              process.fin.data("SIRI"))
 
-
-# set this at the end for all stocks selected
-rownames(final.data)
+rownames(output)<-c("AAPL", "SIRI")
