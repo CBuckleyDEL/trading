cae8da3453f05218e2d0baea1d4b59ac2833354e
shawn0122 <shawnpope@gmail.com>
2014-09-27 19:18:37 -0400

    Create load.packages.R

---
+++ load.packages.R
@@ -0,0 +1,23 @@
+# This script loads the various packages for financial analysis
+# date: 9/27/2014
+
+
+library(XML)
+library(xtable)
+library(PerformanceAnalytics)
+library(quantmod)
+library(stockPortfolio)
+library(quadprog)
+library(DEoptim)
+library(tawny)
+library(xts)
+library(sendmailR)
+library(tseries)
+library(RCurl)
+library(XML)
+library(tis)
+library(RQuantLib)
+library(zoo)
+library(timeDate)
+
+cat("Packages loaded successfully \n")
