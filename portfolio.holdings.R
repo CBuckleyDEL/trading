b84ded65d3f53fbe92dc9b9c4cc6343d77ac9e78
shawn0122 <shawnpope@gmail.com>
2014-10-03 21:03:18 -0400

    Create portfolio.holdings.R

---
+++ portfolio.holdings.R
@@ -0,0 +1,20 @@
+# Define the portfolio Holding
+# need to fully develop this more
+
+
+portfolio <- data.frame(ticker = character(0), shares = numeric(0), stringsAsFactors=F)
+
+portfolio[1,]=c("AAPL", 191)
+portfolio[2,]=c("BAX", 22)
+portfolio[3,]=c("AMZN", 4)
+portfolio[4,]=c("SIRI", 2572)
+portfolio[5,]=c("JNK", 86)
+portfolio[6,]=c("BMY", 125)
+portfolio[7,]=c("DE", 19)
+portfolio[8,]=c("KO", 66)
+portfolio[9,]=c("F", 400)
+portfolio[10,]=c("MCD", 25)
+portfolio[11,]=c("PG", 20)
+portfolio[12,]=c("JNJ", 58)
+portfolio[13,]=c("T", 55)
+portfolio[14,]=c("IP", 40)
