627c99694e48b1dbca0cfa99460a2fd19fe2e266
shawn0122 <shawnpope@gmail.com>
2014-10-21 22:00:43 -0400

    Update portfolio.holdings.R

--- portfolio.holdings.R
+++ portfolio.holdings.R
@@ -3,15 +3,17 @@
 
 # need to add sector analysis
 
+# http://systematicinvestor.wordpress.com/2012/02/13/multiple-factor-model-building-csfb-factors/
+
 # sector ETF's
-tickers = spl('XLY,XLP,XLE,XLF,XLV,XLI,XLB,XLK,XLU')
-tickers.desc = spl('ConsumerCyclicals,ConsumerStaples,Energy,Financials,HealthCare,Industrials,Materials,Technology,Utilities')
+# tickers = spl('XLY,XLP,XLE,XLF,XLV,XLI,XLB,XLK,XLU')
+# tickers.desc = spl('ConsumerCyclicals,ConsumerStaples,Energy,Financials,HealthCare,Industrials,Materials,Technology,Utilities')
 
 
 
 portfolio <- data.frame(ticker = character(0), shares = numeric(0), stringsAsFactors=F)
 
-portfolio[1,]=c("AAPL", 191)
+portfolio[1,]=c("AAPL", 176)
 portfolio[2,]=c("BAX", 22)
 portfolio[3,]=c("AMZN", 4)
 portfolio[4,]=c("SIRI", 2572)
@@ -21,7 +23,9 @@ portfolio[7,]=c("DE", 19)
 portfolio[8,]=c("KO", 66)
 portfolio[9,]=c("F", 400)
 portfolio[10,]=c("MCD", 25)
-portfolio[11,]=c("PG", 20)
+portfolio[11,]=c("PG", 42)
 portfolio[12,]=c("JNJ", 58)
 portfolio[13,]=c("T", 55)
 portfolio[14,]=c("IP", 40)
+portfolio[15,]=c("SPY", 0)
+
